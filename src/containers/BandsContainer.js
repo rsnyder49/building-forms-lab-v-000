38db854be5b7acfe176db3e8862153d4ec5a31b3
Robert Snyder <r.snyder49@gmail.com>
2019-01-18 21:06:54 +0000

    Automatically backed up by Learn

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import BandInput from '../components/BandInput';
+import Bands from '../components/Bands';
 
 class BandsContainer extends Component {
   render() {
