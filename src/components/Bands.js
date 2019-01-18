1ccef523a8d0a60d3796c9b3069848250e355a67
Robert Snyder <r.snyder49@gmail.com>
2019-01-18 21:02:19 +0000

    Automatically backed up by Learn

--- src/components/Bands.js
+++ src/components/Bands.js
@@ -1,7 +1,9 @@
 import React from 'react';
+
  const Bands = props => {
-  const bands = props.bands.map((band, index) => {
-    return <li key={index}>{band.name}</li>;
+const bands = props.bands.map((band, index) => {
+  
+  return <li key={index}>{band.name}</li>;
   });
    return (
     <div>
@@ -9,4 +11,5 @@ import React from 'react';
     </div>
   );
  };
+ 
  export default Bands; 
